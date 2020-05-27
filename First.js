c7f65728304cde6a5a09a32ca7ccf2ad37c530d4
clxiong <chenlong.xiong@avepoint.com>
2020-05-27 17:23:24 +0800

    add file read

--- First.js
+++ First.js
@@ -5,3 +5,21 @@ http.createServer(function(request,respose){
 }).listen(8888);
 
 console.log('server is running...');
+
+var fs = require("fs");
+var data = fs.readFileSync("README.md");
+console.log(data.toString());
+console.log("End..")
+
+
+fs.readFile("README.md", (err,data)=>{
+    if(err)
+    {
+        console.error(err)
+    }
+    else
+    {
+        console.log("The message:"+ data);
+    }
+})
+console.log("sync end..")
